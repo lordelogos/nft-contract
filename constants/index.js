e0750487bcee1e29e2ce693546c72d1cb277d493
Paul OE <paulehiks@gmail.com>
2022-02-17 14:42:10 +0100

    created nft dapp contract

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xB0484dB4711aA34B133BF9713E216A1ad5114E8B";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
